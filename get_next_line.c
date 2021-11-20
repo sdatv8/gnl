81bbe699698b126e727f64ea0f25cb9ff98a82e9
Sarina Jannet <sjannet@pr-f5.kzn.21-school.ru>
2021-11-20 14:30:12 +0300

    double functions

--- get_next_line.c
+++ get_next_line.c
@@ -6,134 +6,71 @@
 /*   By: Sjannet <sjannet@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/24 15:02:33 by Sjannet           #+#    #+#             */
-/*   Updated: 2021/11/20 10:48:26 by Sjannet          ###   ########.fr       */
+/*   Updated: 2021/11/20 14:29:20 by Sjannet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
+#include "get_next_line.h"
 
-size_t	ft_strlen(const char *str)
+char	*get_line(int fd, char *buf)
 {
-	size_t	i;
-
-	i = 0;
-	while (str[i])
-		i++;
-	return (i);
-}
-
-char	*ft_strdup(const char *s)
-{
-	char	*str;
-	int		i;
-
-	i = 0;
-	while (s[i])
-		i++;
-	str = (char *)malloc(sizeof(*s) * (i + 1));
-	if (!str)
-		return (NULL);
-	i = 0;
-	while (s[i])
-	{
-		str[i] = s[i];
-		i++;
-	}
-	str[i] = '\0';
-	return (str);
-}
-
-char	*ft_strjoin(char const *s1, char const *s2)
-{
-	char	*str;
-	int		i;
-	int		j;
-
-	i = 0;
-	j = 0;
-	if (!s1 || !s2)
-		return (NULL);
-	str = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
-	if (str == 0)
-		return (NULL);
-	while (s1[i])
-	{
-		str[i] = s1[i];
-		i++;
-	}
-	while (s2[j])
-	{
-		str[i] = s2[j];
-		j++;
-		i++;
-	}
-	str[i] = '\0';
-	return (str);
-}
-
-
-char	*ft_strchr(const char *s, int c)
-{
-	while (*s && *s != (unsigned char)c)
-		s++;
-	if (*s == (char)c)
-		return ((char *)s);
-	return (NULL);
-}
-
-int		get_next_line(int fd, int BUFFER_SIZE)
-{
+	int			read_line;
+	char		*pointer;
 	char		*line;
-	char		*buf;
+	char		*tmp;
 	static char	*remainder;
-	char	*pointer;
-	int		read_line;
-	int		flag;
 
-	flag = 1;
+	pointer = NULL;
 	if (remainder)
 		line = ft_strdup(remainder);
+	else
+		line = ft_strnew(1);
 
-	buf = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
-	if (!buf)
-		return (0);
-	while (flag && (read_line = read(fd, buf, BUFFER_SIZE)))
+	while (!pointer && (read_line = read(fd, buf,  10)))
 	{
 		buf[read_line] = '\0';
+		tmp = line;
 		line = ft_strjoin(line, buf);
+		free(tmp);
+		// tmp = NULL;
 		if ((pointer = ft_strchr(line, '\n')))
 		{
 			*pointer = '\0';
-			pointer++;
-			remainder = ft_strdup(pointer);
-			flag = 0; //break
+			remainder = ft_strdup(++pointer);
 		}
 	}
-	printf("%s", line);
-
 	
+	if (read_line == 0)
+		return (NULL);
+	return (line);
+}
+
+char	*get_next_line(int fd)
+{
+	char		*line;
+	char		*buf;
 	
-
-
-	return (0);
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (0);
+	buf = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buf)
+		return (NULL);
+	line = get_line(fd, buf);
+	free(buf);
+	// buf = NULL;
+	
+	return (line);
 }
 
 int	main(void)
 {
 	int		fd;
 	int		i;
+	char	*line;
 
 	i = 0;
 	fd = open("text.txt", O_RDONLY);
-	while (i < 20)
-	{
-		get_next_line(fd, 10);
-		printf("\n");
-		i++;
-	}
-	// printf("%s\n", line);
+	while ((line = get_next_line(fd)))
+		printf("%s\n", line);
 	return (0);
 }
